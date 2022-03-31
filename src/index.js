acf545522cb4fc56aa9a496a7bde8c53ef2e323e
Jonathan Moreno <jmoreno@gruposcanner.com>
2022-03-31 00:47:34 -0500

    Agregado Avatar de Usuario

--- src/index.js
+++ src/index.js
@@ -17,16 +17,46 @@ io.on("connection", (socket) => {
 
   socket.on("join:room", (data) => {
     socket.join(data.room);
-    console.log(`El cliente ${socket.username} se ha unido a la sala ${data.room}`);
+    console.log(
+      `El cliente ${socket.username} se ha unido a la sala ${data.room}`
+    );
+    socket.broadcast.to(data.room).emit("user_connected", data);
   });
 
-  socket.on("send:message",({message})=>{
+  socket.on("users", () => {
+    let users = [];
+    io.sockets.sockets.forEach((s) => {
+      users.push(s.username);
+    });
+    socket.emit("users_in_room", users);
+  });
+
+  socket.on("disconnecting", (reason) => {
+    index = 0;
+    for (const room of socket.rooms) {
+      if (index == 0) {
+        index++;
+        continue;
+      }
+      index++;
+      socket.to(room).emit("leave_room", { room, username: socket.username });
+      console.log(`El Usuario ${socket.username} ha salido`);
+    }
+  });
+
+  socket.on("send:message", ({ message }) => {
     message["type"] = "sender";
-    socket.broadcast.to(message.room).emit("message_received",message);
+    socket.broadcast.to(message.room).emit("message_received", message);
   });
 
+  socket.on("logout",()=>{
+    for (const room of socket.rooms) {
+      socket.to(room).emit("leave_room",{room,username:socket.username})
+      socket.leave(room)
+    }
+  })
 });
 
-io.listen(5000);
+io.listen(5001);
 
-console.log("Server Listening on port: ", 5000);
+console.log("Server Listening on port: ", 5001);
